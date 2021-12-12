3231e8f31cb9dd8d74a31dbb368fb83f4d2b7c8b
kongbo <kongbo5582@163.com>
2021-12-12 21:37:42 +0800

    fix: fix

--- serve/config/config.default.js
+++ serve/config/config.default.js
@@ -36,7 +36,7 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: 'localhost',
+      host: '81.68.165.190', // localhost
       // 端口号
       port: '3306',
       // 用户名
